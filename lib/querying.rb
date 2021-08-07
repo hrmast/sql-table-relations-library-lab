3150512710da99a9eb051d11eb3c741f38e89987
Heath Mast <hrmast@gmail.com>
2021-08-07 16:48:03 -0500

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,24 +1,60 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT books.title, books.year
+   FROM books
+   LEFT OUTER JOIN series 
+   ON books.title = series.title 
+   WHERE books.series_id = 1
+   ORDER BY books.year;
+  "
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT characters.name, characters.motto 
+  FROM characters
+  LEFT OUTER JOIN character_books
+  ON characters.id = character_books.character_id
+  ORDER BY characters.motto ASC
+  LIMIT 1;
+  "
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT characters.species, COUNT(characters.species)
+  FROM characters
+  LEFT OUTER JOIN character_books
+  ON characters.species = character_books.character_id
+  GROUP BY characters.species
+  ORDER BY COUNT(characters.species) DESC
+  LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name, subgenres.name
+  FROM authors
+  LEFT OUTER JOIN subgenres
+  ON authors.id = subgenres.id
+  ORDER BY authors.name
+  "
 end
 
-def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+def select_series_title_with_most_human_characters  
+  "SELECT series.title
+  FROM series
+  LEFT OUTER JOIN books ON series.id = books.series_id 
+  LEFT OUTER JOIN character_books ON books.id = character_books.book_id
+  LEFT OUTER JOIN characters ON character_books.character_id = characters.id
+  GROUP BY series.id = characters.species = 'human'
+  ORDER BY series.title;
+  "
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name, COUNT(books.title) 
+  FROM characters
+  LEFT OUTER JOIN character_books ON characters.id = character_books.character_id
+  LEFT OUTER JOIN books ON character_books.book_id = books.id
+  LEFT OUTER JOIN series ON books.series_id = series.id  
+  GROUP BY characters.name 
+  ORDER BY COUNT(books.title) DESC, characters.name ASC;"
 end
